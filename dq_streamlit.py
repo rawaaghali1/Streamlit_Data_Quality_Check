dbc428e325c77bf3b4e4d5c18398bd1bd4dafa1f
ZYing <johnsonying1995@gmail.com>
2023-10-26 17:25:57 +0200

    Update dq_streamlit.py

--- dq_streamlit.py
+++ dq_streamlit.py
@@ -9,7 +9,7 @@ DATA_PATH = ('data/dataset_1.csv')
 
 @st.cache_data
 def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
+    data = pd.read_csv(DATA_PATH, nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
