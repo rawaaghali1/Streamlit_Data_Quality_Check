1ba1802b595be749a22d625232172081bd3ebf23
ZYing <johnsonying1995@gmail.com>
2023-11-03 16:08:54 +0100

    Update dq_streamlit.py

--- dq_streamlit.py
+++ dq_streamlit.py
@@ -200,7 +200,7 @@ with col2:
 	st.text('')
 	st.write('Powered by :')
 with col3:
-	st.image(image, width=100, output_format='PNG')
+	st.image(image, width=80, output_format='PNG')
 
 st.subheader('Metrics')
 
