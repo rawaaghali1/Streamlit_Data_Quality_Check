ece03b114299c68d261f529ceab88250f531da0c
ZYing <johnsonying1995@gmail.com>
2023-11-03 16:09:58 +0100

    Update dq_streamlit.py

--- dq_streamlit.py
+++ dq_streamlit.py
@@ -190,17 +190,16 @@ layout_plot, layout_dist = layout()
 # Heading
 # put logo image on the top right
 image = Image.open('assets/danone_ds_logo.png')
-col1, col2, col3 = st.columns([10, 1, 1])
+col1, col2, col3 = st.columns([9, 1, 1])
 with col1:
 	st.title('Data Quality Dashboard')
 with col2:
 	st.text('')
 	st.text('')
 	st.text('')
-	st.text('')
 	st.write('Powered by :')
 with col3:
-	st.image(image, width=80, output_format='PNG')
+	st.image(image, width=90, output_format='PNG')
 
 st.subheader('Metrics')
 
