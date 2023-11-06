0b9108d24cc197d72a7cfdddbb333b70f53cb878
ZYing <johnsonying1995@gmail.com>
2023-11-06 16:50:49 +0100

    Update dq_streamlit.py

--- dq_streamlit.py
+++ dq_streamlit.py
@@ -560,6 +560,12 @@ with distribution_plot:
     fig.update_layout(layout_dist)
     st.plotly_chart(fig, use_container_width=True)
 
+corr_plot, unknown_plot  = st.columns([1,1])
+with bar_plot:
+    data_quantitative = data[["DIL_VAL", "HUM_VAL", "TPR_VAL", "TIM_VAL"]]
+    fig = px.imshow(data_quantitative.corr(numeric_only=True))
+    st.write(fig, use_container_width=True)
+
 ###### ROW 7 #######
 # signal 
 # st.write('Signal')
