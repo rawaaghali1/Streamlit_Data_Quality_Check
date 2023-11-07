e4f555a2e576ac4034d0361f89bf361be15aca2f
ZYing <johnsonying1995@gmail.com>
2023-11-07 11:00:28 +0100

    Create dq_streamlit_test.py

---
+++ dq_streamlit_test.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import streamlit as st
+from ydata_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
+
+df = pd.read_csv("https://storage.googleapis.com/tf-datasets/titanic/train.csv")
+pr = ProfileReport(df, title="Report")
+st_profile_report(pr)
