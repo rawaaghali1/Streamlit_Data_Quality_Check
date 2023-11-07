358948b0896ef77e642b245da6f8692313f5e8e8
ZYing <johnsonying1995@gmail.com>
2023-11-07 16:01:24 +0100

    Update dq_streamlit_test.py

--- dq_streamlit_test.py
+++ dq_streamlit_test.py
@@ -4,7 +4,7 @@ from ydata_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
 df = pd.read_excel("data/test_raw_file.xlsx")
-if df.shape[0]>30000:
-  df = df.sample(30000)
+if df.shape[0]>10000:
+  df = df.sample(10000)
 pr = ProfileReport(df, title="Report")
 st_profile_report(pr)
