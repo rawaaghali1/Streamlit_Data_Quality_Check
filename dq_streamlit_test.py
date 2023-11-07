1a405f3f446237a20e2ae1d52f8e13d040c93e09
ZYing <johnsonying1995@gmail.com>
2023-11-07 11:20:56 +0100

    Update dq_streamlit_test.py

--- dq_streamlit_test.py
+++ dq_streamlit_test.py
@@ -3,6 +3,6 @@ import streamlit as st
 from ydata_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
-df = pd.read_csv("data/test_raw_file_2.csv")
+df = pd.read_excel("data/test_raw_file.xlsx")
 pr = ProfileReport(df, title="Report")
 st_profile_report(pr)
