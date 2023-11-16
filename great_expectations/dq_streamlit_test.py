379b10b482090f676e15e071695468f5ff0aa737
ZYing <johnsonying1995@gmail.com>
2023-11-16 23:32:57 +0100

    Update dq_streamlit_test.py

--- great_expectations/dq_streamlit_test.py
+++ great_expectations/dq_streamlit_test.py
@@ -356,12 +356,12 @@ def compute_column_checks_results(dq_json):
 	checks = []
 	results = []
 	for i in dq_json:
-		print(i)
+		st.write(i)
 		columns.append(i['column'])
-		print(columns)
+		st.write(columns)
 		#checks.append(ast.literal_eval(i['notes'])[1])
 		checks.append(i['notes'][1])
-		print(checks)
+		st.write(checks)
 		results.append(i['success'])
 	column_results_df = pd.DataFrame({'columns' : columns, 'checks' : checks, 'results' : results})
 	return column_results_df
