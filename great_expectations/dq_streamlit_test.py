5a3b7a99e1d434f7bb5fd26a047ae8b517d1b422
ZYing <johnsonying1995@gmail.com>
2023-11-16 13:38:39 +0100

    Update dq_streamlit_test.py

--- great_expectations/dq_streamlit_test.py
+++ great_expectations/dq_streamlit_test.py
@@ -133,7 +133,7 @@ def load_data(uploaded_file_original, uploaded_file_rule):
 	config = json.load(uploaded_file_rule)
 	data.set_index('RES_NUM', drop = False, inplace = True)
 	return data, config
-@st.cache_data
+
 def perform_dqc(config, dqc):
     merged_df_new = pd.DataFrame()
     for rule in config['rules']:
