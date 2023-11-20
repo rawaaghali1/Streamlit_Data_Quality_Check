333aaf4749db2c7e17d1b8ca09c08870a5609d90
ZYing <johnsonying1995@gmail.com>
2023-11-20 17:24:00 +0100

    Update 1_create_expectations.py

--- great_expectations/pages/1_create_expectations.py
+++ great_expectations/pages/1_create_expectations.py
@@ -23,7 +23,7 @@ if uploaded_file_original is not None:
         # a selection for the user to specify the number of rows
         num_rows = st.slider('Number of rows', min_value=1, max_value=10)
         # columns to lay out the inputs
-        grid = st.columns(4)
+        grid = st.columns(3)
         # Function to create a row of widgets (with row number input to assure unique keys)
         def add_row(row):
             with grid[0]:
@@ -32,11 +32,11 @@ if uploaded_file_original is not None:
                 st.multiselect('Columns', list(data.columns))
             with grid[2]:
                 st.number_input('col3', step=1, key=f'input_col3{row}')
-            with grid[3]:
-                st.number_input('col4', step=1, key=f'input_col4{row}',
-                                value = st.session_state[f'input_col2{row}'] \
-                                    -st.session_state[f'input_col3{row}'],
-                                disabled=True)
+            #with grid[3]:
+            #    st.number_input('col4', step=1, key=f'input_col4{row}',
+            #                    value = st.session_state[f'input_col2{row}'] \
+            #                        -st.session_state[f'input_col3{row}'],
+            #                    disabled=True)
     
         # Loop to create rows of input widgets
         for r in range(num_rows):
