709f571d5dc990c754534be4f8469c6e63018358
ZYing <johnsonying1995@gmail.com>
2023-11-20 17:25:51 +0100

    Update 1_create_expectations.py

--- great_expectations/pages/1_create_expectations.py
+++ great_expectations/pages/1_create_expectations.py
@@ -21,13 +21,13 @@ if uploaded_file_original is not None:
         data = load_data(uploaded_file_original)
         st.write('# Solution using input widgets')
         # a selection for the user to specify the number of rows
-        num_rows = st.slider('Number of rows', min_value=1, max_value=10)
+        num_rows = st.slider('Number of expectations', min_value=1, max_value=10)
         # columns to lay out the inputs
         grid = st.columns(3)
         # Function to create a row of widgets (with row number input to assure unique keys)
         def add_row(row):
             with grid[0]:
-                st.selectbox('Expectations', ('Column values must not be null', 'Column values must be in a list', 'Column'))
+                st.selectbox('Expectations', ('Column values must not be null', 'Column values must be in a list', 'Column values must be of a certain type'))
             with grid[1]:
                 st.multiselect('Columns', list(data.columns))
             with grid[2]:
