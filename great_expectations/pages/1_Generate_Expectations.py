6d8e008259adc275f3245b0c8382816c8b713b5d
ZYing <johnsonying1995@gmail.com>
2023-11-23 15:38:15 +0100

    Update 1_Generate_Expectations.py

--- great_expectations/pages/1_Generate_Expectations.py
+++ great_expectations/pages/1_Generate_Expectations.py
@@ -129,6 +129,8 @@ if uploaded_file_original is not None:
 	
     # Function to append non-form inputs into dataframe
     def add_df():
+        if (st.session_state.input[['Expectations','Columns']].values == [st.session_state.input_df_col1, st.session_state.input_df_col2]).all(axis=1).any():
+                st.warning('The same expectation has already to added on the selected column(s), please delete the existing expectation first.', icon='⚠️')   
         row = pd.DataFrame({'Expectations':[st.session_state.input_df_col1],
                 'Columns':[st.session_state.input_df_col2],
                 'Values':[st.session_state.input_df_col3]})
