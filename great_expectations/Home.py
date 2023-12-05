57bbba11b678bb8aab1e6e066d698e4fe31f4d57
ZYing <johnsonying1995@gmail.com>
2023-12-05 17:18:07 +0100

    Update Home.py

--- great_expectations/Home.py
+++ great_expectations/Home.py
@@ -23,7 +23,7 @@ home_title = "Interactive Data Quality Check"
 home_introduction = """
 Welcome to Interactive Data Quality Check! Data Quality Check, or DQC, refers to the process of validating and ensuring the accuracy, consistency, and reliability of data in your data. This process is crucial in data management and analytics because high-quality data is essential for making informed decisions and accurate analyses. 
 \n
-To help you ensure the quality of your data, we developed this interactive DQC Tool, a web app where you can upload your raw data, define your expectations of what certain column values in your data should be like, and generate a dashboard to review your data quality and even explore your raw data.
+To help you ensure the quality of your data, we developed this interactive DQC Tool, a web app where you can upload your raw data, define your rules (or expectations) of what certain column values in your data should be like, and generate a dashboard to review your data quality and even explore your raw data.
 """
 
 home_howitworks = """
@@ -32,14 +32,15 @@ Your journey towards impeccable data quality begins here! Interactive Data Quali
 ##### 1. Generate Your Data Quality Dashboard
 
 - **Upload Your Data**: Begin by uploading your dataset in a supported format, such as CSV or Excel. Please make sure that your file only contains one sheet and one header row on top.
-- **Upload Your Expectation JSON File**: Provide us with a JSON file that outlines your specific expectations for data quality. This file should contain criteria such as data types, desired ranges, uniqueness constraints, and more. If you don't have such a file, you can generate one in the "Create Your Expectation JSON File" step.
-- **Receive Insights**: Once you submit your data and expectation file, you'll receive a comprehensive dashboard visualizing the quality of your data, highlighting areas that meet your criteria and those that need attention.
-- **Download your Excel Report**: Once you submit your data and expectation file, you can also choose to download an Excel report that includes the index of column values that don't match your expectations.
+- **Upload Your JSON File for DQC Rules**: Upload a JSON file that outlines your specific rules for data quality check. This file should contain criteria such as data types, desired ranges, uniqueness constraints, and more. If you don't have such a file, you can generate one in the "Create Your DQC Rules" step.
+- **Receive Insights**: Once you submit your data and your JSON file for DQC rules, you'll receive a comprehensive dashboard visualizing the quality of your data, highlighting areas that meet your criteria and those that need attention.
+- **Download your Excel Report**: Once you submit your data and your JSON file for DQC rules, you can also choose to download an Excel report that includes the index of column values that don't match your rules.
 
-##### 2. Create Your Expectation JSON File
+##### 2. Create Your DQC Rules
 
-- **Upload Your Data and Define Expectations**: If you don't know about JSON files, no worries! Simply upload your CSV or Excel dataset, and our intuitive interface will guide you through setting up your expectations for each column. This could include specifications like acceptable value ranges, required formats, or uniqueness.
-- **Generate and Download Your JSON File**: After setting your expectations, hit 'Download the list as a json file'. Instantly, you'll have a tailor-made JSON file that you can use with our app in the next step.
+- **Upload Your Data**: Upload your CSV or Excel dataset, and our tool will read the column names in your data for the next steps.
+- **Define Your DQC Rules**: Our intuitive interface will guide you through setting up your DQC rules for any column. This could include specifications like acceptable value ranges, required formats, or uniqueness.
+- **Generate and Download Your JSON File**: After setting your rules, hit 'Download the list as a json file'. Instantly, you'll have a tailor-made JSON file that you can use with our app in "DQC Dashboard".
 """
 
 st.markdown("""\n""")
